c82183f71947dd370c7904e33008c76a7217c7d0
Claudio Gimenez <cgimenez@fi.uba.ar>
2020-07-15 16:42:38 -0300

    agrego consulta de funciones para punto 2 falta terminar

---
+++ consulta_funciones.py
@@ -0,0 +1,70 @@
+import reuti_codigo
+
+def listar_funciones():
+    
+    archivo_codigo = open('salida_codigo.csv','r')
+    funciones = reuti_codigo.armar_lista(archivo_codigo)
+    numero=0
+    
+    while len(funciones)>numero:
+        #for i in range(0,2):
+            #print(funciones[numero] ,sep="\t", end="|")
+        #columnas=reuti_codigo.generar_texto_encolumnado(40,funciones[numero])
+        #texto=columnas[0]
+        texto=''
+        for i in range(0,2):
+            columnas=reuti_codigo.generar_texto_encolumnado(40,funciones[numero])
+            texto=texto+columnas[0]
+            numero+=1
+        print (texto)
+        
+        #funciones = reuti_codigo.armar_diccionario(funciones)
+    archivo_codigo.close()
+    return(funciones)
+    #return lista_funciones
+
+def validar_opcion(opcion):
+    valido = False
+    nombre_funcion = ''
+    if opcion[-1] == '?' or opcion[-1] == '#':
+        valido = True
+        nombre_funcion = opcion[-1]
+        
+    elif opcion[-5:] == '?todo' or opcion[-5:] == '#todo'
+        valido = True
+        nombre_funcion = opcion[:-5]
+        
+    elif opcion[-14:] ==  'imprimir ?todo':
+        valido = True
+        nombre_funcion = opcion[:-14]
+        
+    return valido,nombre_funcion
+
+def validar_nombre_funcion(nombre_funcion,l_funciones):
+    posicion = 0
+    encontrado = False
+    nombre_encontrado=''
+    while posicion < len(l_funciones) or not encontrado:
+        if l_funciones[0] in nombre_funcion:
+            encontrado = True
+            nombre_encontrado=l_funciones[0]
+        posicion+=1
+    return encontrado,l_funciones[0]    
+        
+def ingresar_opcion(funciones):
+    opcion = input("Función: ")
+    #encontrado,nombre_funcion = validar_nombre_funcion(nombre,funciones)
+    #opcion = nombre[nombre.find(nombre_funcion)+len(nombre_funcion):]
+    while opcion!='':
+        if not validar_opcion(opcion):
+            print('Caracter invalido')
+        elif not     
+        opcion = input("Funcion: ")
+    return opcion
+    #print(opcion)
+
+#print(listar_funciones())
+funciones = listar_funciones()
+ingresar_opcion(funciones)
+    
+
