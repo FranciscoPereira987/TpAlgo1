fe8b14ef2469fe180b643b66d9e862f7e50e853c
claudio <claudioalbertogimenez@gmail.com>
2020-07-18 11:47:10 -0300

    Update consulta_funciones.py

--- consulta_funciones.py
+++ consulta_funciones.py
@@ -69,8 +69,9 @@ def leer(archivo):
     return devolver
 
 
-def mostrar_informacion(miopcion,nombre_encontrado):
+def mostrar_informacion(l_funcion,l_comentarios,nombre_encontrado):
     #abro archivos y los paso a listas
+    '''
     archivo_codigo = open('salida_codigo.csv','r')
     archivo_comentarios = open('salida_comentarios.csv','r')
     l_funcion = []
@@ -82,32 +83,33 @@ def mostrar_informacion(miopcion,nombre_encontrado):
     for linea in archivo_comentarios:
         comentarios = linea.rstrip("\n").split(",")
         l_comentarios.append(comentarios)
+    '''
+    #if miopcion == '?':
+    parametros = ''
+    modulo = ''
+    contador_funciones = 0
+    while contador_funciones < len(l_funcion) and nombre_encontrado \
+    != l_funcion[contador_funciones][0] + "." + l_funcion[contador_funciones][2]:
+        contador_funciones+=1
+    parametros = l_funcion[contador_funciones][1]
+    modulo = l_funcion[contador_funciones][2]
     
-    if miopcion == '?':
-        parametros = ''
-        modulo = ''
-        contador_funciones = 0
-        while contador_funciones < len(l_funcion) and nombre_encontrado \
-        != l_funcion[contador_funciones][0] + "." + l_funcion[contador_funciones][2]:
-            contador_funciones+=1
-        parametros = l_funcion[contador_funciones][1]
-        modulo = l_funcion[contador_funciones][2]
-        
-        autor = ''
-        ayuda = ''
-        contador_comentarios = 0
-        while contador_comentarios < len(l_comentarios) and nombre_encontrado[:nombre_encontrado.find(".")] \
-        != l_comentarios[contador_comentarios][0]:
-            contador_comentarios+=1
-        autor = l_comentarios[contador_comentarios][1]
-        ayuda = l_comentarios[contador_comentarios][2]
-        
+    autor = ''
+    ayuda = ''
+    contador_comentarios = 0
+    while contador_comentarios < len(l_comentarios) and nombre_encontrado[:nombre_encontrado.find(".")] \
+    != l_comentarios[contador_comentarios][0]:
+        contador_comentarios+=1
+    autor = l_comentarios[contador_comentarios][1]
+    ayuda = l_comentarios[contador_comentarios][2]
+            
     print(l_funcion)
     print(l_comentarios)
-    print(ayuda,'parametros: ',parametros,'modulo: ',modulo,autor)
+    return parametros,modulo,autor,ayuda
+    #print(ayuda,'parametros: ',parametros,'modulo: ',modulo,autor)
     
-    archivo_codigo.close()
-    return l_funcion, l_comentarios    
+    #archivo_codigo.close()
+    #return l_funcion, l_comentarios    
     
     
 
@@ -119,7 +121,21 @@ def ingresar_opcion(funciones):
         opcion_elegida,nombre_funcion,miopcion=validar_opcion(opcion)
         #encontrado,nombre_encontrado = validar_nombre_funcion(nombre_funcion,funciones)
         
-        if opcion_elegida:
+        if opcion_elegida: #si la opcion elegida es valida
+            
+            #abro archivos y los paso a listas
+            archivo_codigo = open('salida_codigo.csv','r')
+            archivo_comentarios = open('salida_comentarios.csv','r')
+            l_funcion = []
+            l_comentarios = []
+    
+            for linea in archivo_codigo:
+                funcion = linea.rstrip("\n").split(",")
+                l_funcion.append(funcion)
+            for linea in archivo_comentarios:
+                comentarios = linea.rstrip("\n").split(",")
+                l_comentarios.append(comentarios)
+            
             if miopcion == '?todo' or miopcion == '#todo' or miopcion == 'imprimir ?todo':
                 #aca muestro todo de todas las funciones segun la opcion # o ? o imprimirtodo
                 print('aca muestro o todo o genero ayuda_funciones.txt')
@@ -128,9 +144,24 @@ def ingresar_opcion(funciones):
                 encontrado, nombre_encontrado = validar_nombre_funcion(nombre_funcion, funciones)
                 if encontrado:
                     print("Aca muestro las cosas de una funcion")
-                    mostrar_informacion(miopcion,nombre_encontrado)
+                    if miopcion == '?':
+                        parametros,modulo,autor,ayuda = mostrar_informacion(l_funcion,l_comentarios,nombre_encontrado)
+                        #print(ayuda,'parametros: ',parametros,'modulo: ',modulo,autor)
+                        if ayuda:
+                            print(ayuda)
+                        else:
+                            print('No hay ayuda para esta funcion')
+                        if autor:
+                            print(autor)
+                        else:
+                            print('No hay autor para esta funcion')
+                        print('Parametros:',parametros)
+                        print('Modulo:',modulo)
+                        
                 else:
                     print("La funcion es incorrecta")
+            archivo_codigo.close()
+            archivo_comentarios.close()
         else:
             print("opcion invalida")
         
