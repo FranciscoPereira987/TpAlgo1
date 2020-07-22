541f047341cb5b25cbbaf463a58eaee1d58f54cd
Naicolass <67443138+Naicolass@users.noreply.github.com>
2020-07-22 07:48:17 -0300

    Update panel_general_de_funciones_sin_terminar.py
    Update de panel general, falta crear lista de cantidad de veces que se llama cada funcion y con ello terminar de rellenar los datos.

--- panel_general_de_funciones_sin_terminar.py
+++ panel_general_de_funciones_sin_terminar.py
@@ -1,17 +1,12 @@
 import reuti_codigo
-from reuti_codigo import leer_linea_archivo
+
 import csv
-#voy a cambiar el metodo de impresion del archivo hoy despues de clase
-#falta buscar cantidad de returns, si tiene comentarios
-#falta agregar autor, ayuda
-#falta ordenar la impresion
-#falta ver si tiene comentarios cada funcion
-#necesito ayuda con como ver si tiene ayuda y comentarios
+#Falta contar invocaciones de funciones, guardarlo en el csv e imprimirlo
+#Falta cambiar nombre final de fuente_unico.csv y comentarios.csv en las funciones para buscar los parametros para la versión final
 def armar_lista_parametros(archivo_codigo):
     """
     [Autor: Nicolas Valenzuela]
-    [Ayuda: Recibe el archivo csv, arma una lista con la cantidad de parametros
-    devuelve la lista armada con la cantidad de parametros por funcion]
+    [Ayuda: Recibe el archivo csv, arma una lista con la cantidad de parametros y devuelve la lista armada con la cantidad de parametros por funcion]
     """
     lista_funciones = []
     linea = reuti_codigo.leer_linea_archivo(archivo_codigo) 
@@ -27,16 +22,16 @@ def armar_lista_parametros(archivo_codigo):
         else:
                 lista_funciones.append(x)
 
-        linea = leer_linea_archivo(archivo_codigo)
+        linea = reuti_codigo.leer_linea_archivo(archivo_codigo)
 
     archivo_codigo.seek(0)
 
     return lista_funciones
+
 def cantidad_de_lineas():
     """
     [Autor: Nicolas Valenzuela]
-    [Ayuda: Arma una lista con la cantidad de lineas
-    devuelve la lista armada con la cantidad de lineas por funcion]
+    [Ayuda: Arma una lista con la cantidad de lineas y devuelve la lista armada con la cantidad de lineas por funcion]
     """
     archivo_codigo = open('salida_codigo.csv','r')
     palabra = ','   
@@ -49,84 +44,13 @@ def cantidad_de_lineas():
                 ocurrencias.append(0)
     archivo_codigo.close()            
     return ocurrencias
-def lineas_codigo(archivo_codigo):
 
-    lista_lineas = []
-    linea = leer_linea_archivo(archivo_codigo) 
-                                                      
-    while linea != ['']:
-        funcion = f'{linea[3]}'
-                                       
-        lista_funciones += [funcion]
-        linea = leer_linea_archivo(archivo_codigo)
 
-    archivo_codigo.seek(0)
-
-    return lista_funciones
-
-def cantidad_de_if(archivo_codigo):
-    palabra = ' if '
-    palabra2 = ' elif '
-    ocurrencias = []
-  
-    for row in archivo_codigo:
-            if palabra in row or palabra2 in row:
-                x = row.count(palabra)
-                x = x + row.count(palabra2)
-                ocurrencias.append(x)
-            else:
-                ocurrencias.append(0)
-    return ocurrencias
-def cantidad_de_for():
-    archivo_codigo = open('salida_codigo.csv','r')
-    palabra = ' for '   
-    ocurrencias = []
-    for lineas in archivo_codigo:
-            if palabra in lineas :
-                x = lineas.count(palabra)                
-                ocurrencias.append(x)
-            else:
-                ocurrencias.append(0)
-    archivo_codigo.close()            
-    return ocurrencias
-def cantidad_de_while():
-    archivo_codigo = open('salida_codigo.csv','r')
-    palabra = ' for '   
-    ocurrencias = []
-    for lineas in archivo_codigo:
-            if palabra in lineas :
-                x = lineas.count(palabra)                
-                ocurrencias.append(x)
-            else:
-                ocurrencias.append(0)
-    archivo_codigo.close()            
-    return ocurrencias
-def cantidad_de_break():
-    archivo_codigo = open('salida_codigo.csv','r')
-    palabra = ' break '   
-    ocurrencias = []
-    for lineas in archivo_codigo:
-            if palabra in lineas :
-                x = lineas.count(palabra)                
-                ocurrencias.append(x)
-            else:
-                ocurrencias.append(0)
-    archivo_codigo.close()            
-    return ocurrencias
-
-def cantidad_de_exit():
-    archivo_codigo = open('salida_codigo.csv','r')
-    palabra = ' exit '   
-    ocurrencias = []
-    for lineas in archivo_codigo:
-            if palabra in lineas :
-                x = lineas.count(palabra)                
-                ocurrencias.append(x)
-            else:
-                ocurrencias.append(0)
-    archivo_codigo.close()            
-    return ocurrencias
 def lista_ayuda():
+    """
+    [Autor: Nicolas Valenzuela]
+    [Ayuda: Busca si las funciones tiene ayuda y devuelve la lista armada con si o no dependiendo la respuesta]
+    """
     archivo_codigo = open('salida_comentarios.csv','r')
     lista_ayuda = []
     for i in archivo_codigo:
@@ -139,100 +63,145 @@ def lista_ayuda():
     archivo_codigo.seek(0)
     archivo_codigo.close
     return lista_ayuda
+
 def funcion_autor():
+    """
+    [Autor: Nicolas Valenzuela]
+    [Ayuda: Busca autores de funcion y devuelve una lista con sus respectivos nombres]
+    """
     archivo_codigo = open('salida_comentarios.csv','r')
     lista_autores = []
-    linea = leer_linea_archivo(archivo_codigo) 
+    linea = reuti_codigo.leer_linea_archivo(archivo_codigo) 
                                                       
     while linea != ['']:
         funcion = f'{linea[1]}'                                    
         lista_autores += [funcion]
-        linea = leer_linea_archivo(archivo_codigo)
+        linea = reuti_codigo.leer_linea_archivo(archivo_codigo)
 
     archivo_codigo.seek(0)
     
     return lista_autores
 
+def comentarios_extra():
+    """
+    [Autor: Nicolas Valenzuela]
+    [Ayuda: Busca si hay comentarios extra aparte de ayuda autor y devuelve una lista con la cantidad de lineas extra]
+    """
+    archivo_codigo=open('salida_comentarios.csv','r')
+    lista_extra=[]
+    for row in archivo_codigo:
+        x= row.count(',')
+        
+        if x > 2:
+            x = x -2
+            lista_extra.append(x)
+        else:
+            lista_extra.append(0)
+    return(lista_extra)
 
-def listar_todo():
-    numero = 0
+
+def cantidad_de(palabras):
+    """
+    [Autor: Nicolas Valenzuela]
+    [Ayuda: Recibe una lista de palabras ( o palabra ) y busca cuantas veces se repite por cada funcion enviando una lista con el resultado]
+    """
+    archivo_codigo=open('salida_codigo.csv','r')
+    ocurrencias = []
+    for row in archivo_codigo:
+        x=0
+        for palabra in palabras:
+            if palabra in row:
+                x = x + row.count(palabra)
+                
+        ocurrencias.append(x)
+            
+    return ocurrencias
+
+
+def crear_csv():
+    """
+    [Autor: Nicolas Valenzuela]
+    [Ayuda: Crea archivo panel_general.csv y guarda sus respectivos datos en los campos]
+    """
     archivo_codigo = open('salida_codigo.csv','r')
-    archivo_codigo2=open('salida_comentarios.csv','r')
     funciones = reuti_codigo.armar_lista(archivo_codigo)
-    parametros= armar_lista_parametros(archivo_codigo)
+    parametr= armar_lista_parametros(archivo_codigo)    
     cant_lineas= cantidad_de_lineas()
-    cant_if=cantidad_de_if(archivo_codigo)
-    cant_for=cantidad_de_for()
-    cant_while=cantidad_de_while()
-    cant_break=cantidad_de_break()
-    cant_exit=cantidad_de_exit()
+    cant_return=cantidad_de([' return '])        
+    cant_if=cantidad_de([' if ',' elif '])
+    cant_for=cantidad_de([' for '])
+    cant_while=cantidad_de([' while '])
+    cant_break=cantidad_de([' break '])
+    cant_exit=cantidad_de([' exit '])   
+
+    ayuda=lista_ayuda()
     autores=funcion_autor()
-   # ayuda=lista_ayuda()
-    print (autores)
-    while len(funciones)>numero :
-        texto=''
-        texto2=''
-        texto3=''
-        texto4=''
-        texto5=''
-        texto6=''
-        texto7=''
-        texto8=''
-        columnas=reuti_codigo.generar_texto_encolumnado(40,funciones[numero])
-        columnas2=reuti_codigo.generar_texto_encolumnado(2, str(parametros[numero]))
-        columnas3=reuti_codigo.generar_texto_encolumnado(2,str(cant_if[numero]))
-        columnas4=reuti_codigo.generar_texto_encolumnado(2,str(cant_for[numero]))
-        columnas5=reuti_codigo.generar_texto_encolumnado(2,str(cant_while[numero]))
-        columnas6=reuti_codigo.generar_texto_encolumnado(2,str(cant_lineas[numero]))
-    #    columnas7=reuti_codigo.generar_texto_encolumnado(20,ayuda[numero])
-     #   columnas8=reuti_codigo.generar_texto_encolumnado(20,autores[numero])
-        texto=texto+columnas[0]
-        texto2=texto2+columnas2[0]
-        texto3=texto3+columnas3[0]
-        texto4=texto4+columnas4[0]
-        texto5=texto5+columnas5[0]
-        texto6=texto6+columnas6[0]
-     #   texto7=texto7+columnas7[0]
-     #   texto8=texto8+columnas8[0]
-        print(texto,texto2,texto3,texto4,texto5,texto6)#,texto8)#,texto7)
-        numero2= 0
-        numero += 1
-
-  #  print (numero,"funciones")
+    coment_extra=comentarios_extra()
     
-        
-        
-    archivo_codigo.close()
-    archivo_codigo2.close()
-    
-    
-    return (funciones,parametros,cant_if,cant_for,cant_while,cant_break,cant_exit,cant_lineas,autores)#,ayuda)
-
-
-
-
-
-def crear_csv(funciones,parametr,cant_if,cant_for,cant_while,cant_break,cant_exit,cant_lineas,autores,ayuda):
-    
-   
     with open('panel_general.csv', 'w', newline='') as miarchivo:
         parametros = ['FUNCION','Parámetros','Líneas','Invocaciones','Returns','if/elif','for','while','break','Exit','Coment','Ayuda','Autor']
         escribir = csv.DictWriter(miarchivo, fieldnames=parametros)
         escribir.writeheader()
         for i in range(len(funciones) ):
             
-            escribir.writerow({'FUNCION': funciones[i], 'Parámetros' : parametr[i], 'Líneas' : cant_lineas[i], 'if/elif' : cant_if[i],
+            escribir.writerow({'FUNCION': funciones[i], 'Parámetros' : parametr[i], 'Líneas' : cant_lineas[i], 'Returns': cant_return[i], 'if/elif' : cant_if[i],
                                'for' : cant_for[i], 'while' : cant_while[i], 'break' : cant_break[i], 'Exit' : cant_exit[i],
-                               'Ayuda' : ayuda[i], 'Autor' : autores[i]})
-
+                               'Coment' : coment_extra[i], 'Ayuda' : ayuda[i], 'Autor' : autores[i]})
+            
+        archivo_codigo.close
         return ()
-        
-        
 
+def listar_(i):
+    """
+    [Autor: Nicolas Valenzuela]
+    [Ayuda: Recibe la columna a imprimir y devuelve una lista con todos los parametros de la dicha columna]
+    """
+    archivo_codigo = open('panel_general.csv','r')
+    lista_ = []
+    linea = reuti_codigo.leer_linea_archivo(archivo_codigo) 
+                                                      
+    while linea != ['']:
+        funcion = f'{linea[i]}'                                    
+        lista_ += [funcion]
+        linea = reuti_codigo.leer_linea_archivo(archivo_codigo)
+
+    archivo_codigo.seek(0)
+    archivo_codigo.close
+    
+    return lista_
+
+def listar_todos():
+    """
+    [Autor: Nicolas Valenzuela]
+    [Ayuda: Imprime por pantalla el archivo panel_general.csv en orden]
+    """
+    numero=0
+    while len(listar_(0))>numero :
         
+        columnas=reuti_codigo.generar_texto_encolumnado(45,str(listar_(0)[numero]))
+        columnas2=reuti_codigo.generar_texto_encolumnado(10,str(listar_(1)[numero]))
+        columnas3=reuti_codigo.generar_texto_encolumnado(5,str(listar_(2)[numero]))
+        columnas4=reuti_codigo.generar_texto_encolumnado(13,str(listar_(3)[numero]))
+        columnas5=reuti_codigo.generar_texto_encolumnado(6,str(listar_(4)[numero]))
+        columnas6=reuti_codigo.generar_texto_encolumnado(7,str(listar_(5)[numero]))
+        columnas7=reuti_codigo.generar_texto_encolumnado(3,str(listar_(6)[numero]))
+        columnas8=reuti_codigo.generar_texto_encolumnado(5,str(listar_(7)[numero]))
+        columnas9=reuti_codigo.generar_texto_encolumnado(5,str(listar_(8)[numero]))
+        columnas10=reuti_codigo.generar_texto_encolumnado(4,str(listar_(9)[numero]))
+        columnas11=reuti_codigo.generar_texto_encolumnado(6,str(listar_(10)[numero]))
+        columnas12=reuti_codigo.generar_texto_encolumnado(5,str(listar_(11)[numero]))
+        columnas13=reuti_codigo.generar_texto_encolumnado(25,str(listar_(12)[numero]))
+        
+        numero2= 0
+        numero += 1
+        
+        print(columnas[0],columnas2[0],columnas3[0],columnas4[0],columnas5[0],columnas6[0],columnas7[0],columnas8[0],columnas9[0],columnas10[0],columnas11[0],columnas12[0],columnas13[0])
+        
+    return ()
 
 
-funciones, parametros,cant_if,cant_for,cant_while,cant_break,cant_exit,cant_lineas, autores=listar_todo()
-ayuda=lista_ayuda()
-crear_csv(funciones,parametros,cant_if,cant_for,cant_while,cant_break,cant_exit,cant_lineas,autores,ayuda)
+crear_csv()
+
+listar_todos()
+
 
