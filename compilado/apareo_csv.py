2230ae2f6bf751c9fe11d2d92d1a0ec7d8d325d2
Francisco <franciscopere987@gmail.com>
2020-07-23 17:38:43 -0300

    compilado de archivos del tp

---
+++ compilado/apareo_csv.py
@@ -0,0 +1,53 @@
+SALIDA = "temporal_apareo.txt"
+
+
+def grabar_csva(l_lineas, archivo): # CAMBIAR NOMBRE A ESTA  C O S A..
+    """
+        [Autor: Ivan Coronel]
+        [Ayuda: Recibe una lista con lineas correspondientes a
+                dos archivos distintos y un manejador de archivos.
+                Utiliza el manejador para grabar el archivo
+                en formato csva.
+    """
+    for i in range(len(l_lineas) - 1 ):
+        archivo.write(",".join(l_lineas[i]) + ';')
+    archivo.write(",".join(l_lineas[-1]) + '\n')
+
+
+def aparear_csv(t_archivo_1, t_archivo_2, clave_1, clave_2):
+    """
+        [Autor: Ivan Coronel]
+        [Ayuda: Recibe el nombre de dos archivos csv y las
+                posiciones de sus claves.
+                Los procesa, graba un archivo con la informacion
+                apareada y devuelve el nombre del archivo
+                de salida y su cantidad de lineas.
+    """
+    grabados = 0
+    with open(t_archivo_1, "r") as archivo_1,\
+            open(t_archivo_2, "r") as archivo_2,\
+            open(SALIDA, "w") as salida:
+                linea_ar_1 = archivo_1.readline().strip('\n').split(',')     
+                linea_ar_2 = archivo_2.readline().strip('\n').split(',')     
+                while linea_ar_1 != [''] and linea_ar_2 != ['']:
+                    if linea_ar_1[clave_1] == linea_ar_2[clave_2]:
+                        grabar_csva([linea_ar_1, linea_ar_2], salida) 
+                        linea_ar_1 = archivo_1.readline().strip('\n').split(',')     
+                        linea_ar_2 = archivo_2.readline().strip('\n').split(',')     
+                        grabados += 1
+                        
+
+                    elif linea_ar_1[clave_1] > linea_ar_2[clave_2]:
+                        linea_ar_2 = archivo_2.readline().strip('\n').split(',')     
+                    elif linea_ar_1[clave_1] < linea_ar_2[clave_2]:
+                        grabar_csva([linea_ar_1, ['']] , salida) 
+                        linea_ar_1 = archivo_1.readline().strip('\n').split(',')     
+                        grabados += 1
+                 
+                while linea_ar_1 != ['']:
+                    grabar_csva([linea_ar_1, ['']], salida) 
+                    linea_ar_1 = archivo_1.readline().strip('\n').split(',')     
+                    grabados += 1
+
+    return SALIDA, grabados
+
---
+++ compilado/principal.py
@@ -0,0 +1,121 @@
+import ordenamiento, mezcla, os, reuti_codigo
+import consulta_funciones, arbol_invo, info_participacion
+import panel_general_de_funciones
+
+def limpiar_archivos(lista_codigos, lista_comentarios):
+    """
+    [Autor: Francisco Pereira]
+    [Ayuda: Elimina archivos luego de que hayan sido mergeados]
+    """
+    lista = lista_codigos + lista_comentarios
+    for archivo in lista:
+        os.remove(archivo)
+
+def listar_archivos(archivo_con_rutas):
+    """
+    [Autor: Francisco Pereira]
+    [Ayuda: Lee un archivo que contiene rutas
+    y coloca las mismas en una lista]
+    """
+    lista_rutas = []
+    archivo = open(archivo_con_rutas, 'r')
+    linea = archivo.readline()
+
+    while linea:
+        lista_rutas.append(linea.rstrip('\n'))
+        linea = archivo.readline()
+
+    archivo.close() 
+
+    return lista_rutas
+
+def generar_fuente_y_com():
+    """
+    [Autor: Francisco Pereira]
+    [Ayuda: Genera los archivos fuente_unico y comentarios
+    con el formato pedido]
+    """
+    comentarios, codigo = ordenamiento.main_ordenamiento()
+    lista_cod = listar_archivos(codigo)
+    lista_com = listar_archivos(comentarios)
+    mezcla.mezclar_archivos(lista_cod, 'fuente_unico.csv', 0)
+    mezcla.mezclar_archivos(lista_com, 'comentarios.csv', 0)
+    lista_cod.append(codigo)
+    lista_com.append(comentarios)
+    limpiar_archivos(lista_cod, lista_com)
+
+
+def esperar():
+    """
+    [Autor: Francisco Pereira]3
+    [Ayuda: espera a que el usuario ingrese
+    algo para que se continue con la ejecutcion del programa]
+    """
+    print("\n\n")
+    print("Ingrese cualquier comando para regresar al menu")
+    input(">>>")
+
+def ejecutar(opcion):
+    """
+    [Autor: Francisco Pereira]
+    [Ayuda: direcciona la opcion elegida para que
+    el usuario obtenga la salida esperada
+    ]
+    """
+    if opcion == '1':
+        panel_general_de_funciones.crear_csv()
+        panel_general_de_funciones.listar_todos()
+        esperar()
+
+    elif opcion == '2':
+        funciones = consulta_funciones.listar_funciones()
+        consulta_funciones.ingresar_opcion(funciones)
+
+    elif opcion == '3':
+        reuti_codigo.generar_analizador()
+        esperar()
+        
+    elif opcion == '4':
+        arbol_invo.arbol_invocacion()
+        esperar()
+        
+
+    elif opcion == '5':
+        info_participacion.informar_participacion()
+        esperar()
+    
+    elif opcion == 'todavia no se ingreso nada como opcion':
+        print("Bienvenido al programa!!!!!")
+
+    else:
+        print("No se eligio una opcion correcta")
+
+def menu(display):
+    """
+    [Autor: Francisco Pereira]
+    [Ayuda: menu del programa]
+    """
+    opcion = 'todavia no se ingreso nada como opcion'
+    while opcion != '6':
+        ejecutar(opcion)
+        print(display)
+        opcion = input(">>> ")
+    
+
+def analizador_estructurada():
+    """
+    [Autor: Francisco Pereira]
+    [Ayuda: Funcion principal del programa]
+    """
+    generar_fuente_y_com()
+    display = "1-Panel general de funciones.\n"
+    display += "2-Consulta de funciones.\n"
+    display += "3-Analizador de reutilizacion de codigo.\n"
+    display += "4-Arbol de invocacion.\n"
+    display += "5-Informacion por desarrolador.\n"
+    display += "6-Cerrar programa."
+    menu(display)
+
+if __name__ == '__main__':
+    analizador_estructurada()
+
